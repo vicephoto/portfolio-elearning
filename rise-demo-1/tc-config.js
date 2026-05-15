32b75b6c1000a3aedb402147cf6a30f156cc259e
vicephoto <82692173+vicephoto@users.noreply.github.com>
2026-05-16 01:36:58 +0200

    Add demo courses

---
+++ rise-demo-1/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://lHEGp3u6umX7tkRpaUzcvWIiaYDwdnw7_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "UD1 - Los hidratos de carbono"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": ""
+};
---
+++ rise-demo-2/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://fl-uCJk0jzXIkFHDk-2T36IoID-nmNJm_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "UD1 - Bases y teorías de la neuroeducación"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": ""
+};
