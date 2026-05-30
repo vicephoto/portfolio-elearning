4e8d40a4ffb78f878e6f48cce0e6344e10f86cce
vicephoto <82692173+vicephoto@users.noreply.github.com>
2026-05-30 20:57:24 +0200

    Add J1 visa course

---
+++ storyline-demo-3/story_content/triggers.js
@@ -0,0 +1,32 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6MyyHS2ynIm":
+        Script1();
+        break;
+      case "68W556BpxCJ":
+        Script2();
+        break;
+      case "654NjPUT8XT":
+        Script3();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
---
+++ storyline-demo-3/story_content/user.js
@@ -0,0 +1,34 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  window.top.location.href = "https://exchanges.skilled.me/mod/quiz/view.php?id=174";
+
+}
+
+window.Script2 = function()
+{
+  window.top.location.href = "https://exchanges.skilled.me/mod/quiz/view.php?id=174";
+
+}
+
+window.Script3 = function()
+{
+  window.top.location.href = "https://exchanges.skilled.me/mod/quiz/view.php?id=156";
+
+}
+
+};
